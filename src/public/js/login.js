6017b1e27c083612090349cb71550570920b7fd6
emirubinsky <rubinskyemiliano@gmail.com>
2024-04-10 13:59:22 -0300

    Mantener la sesion del usuario activa, arreglo de endpoints

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,28 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('loginForm');
-
-    if (loginForm) {
-        loginForm.addEventListener('submit', function (event) {
-            event.preventDefault();
-
-            const formData = new FormData(loginForm);
-
-            fetch('http://localhost:8080/api/users/login', {
-                method: 'POST',
-                body: formData,
-            })
-                .then(response => {
-                    if (response.ok) {
-                        // Si la respuesta es exitosa, redirigir al usuario al inicio
-                        console.log("Inicio de sesión exitoso!")
-                    } else {
-                        // Si la respuesta no es exitosa, mostrar un mensaje de error
-                        console.error('Error en el inicio de sesión');
-                    }
+    document.addEventListener('DOMContentLoaded', function () {
+        const registerForm = document.getElementById('registerForm');
+    
+        if (registerForm) {
+            registerForm.addEventListener('submit', function (event) {
+                event.preventDefault();
+    
+                const formData = new FormData(registerForm);
+                const errorMessage = document.getElementById('errorMessage');
+    
+                fetch('http://localhost:8080/api/users/register', {
+                    method: 'POST',
+                    body: formData,
                 })
-                .catch(error => {
-                    console.error('Error en el inicio de sesión:', error);
-                });
-        });
-    }
-});
+                    .then(response => {
+                        if (response.ok) {
+                            // Si la respuesta es exitosa, redirigir al usuario al inicio
+                            console.log("Registro de usuario exitoso!")
+                        } else {
+                            // Si la respuesta no es exitosa, mostrar un mensaje de error
+                            errorMessage.textContent = 'Este email ya tiene cuenta.';
+                            errorMessage.style.display = 'block';
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error en el registro:', error);
+                    });
+            });
+        }
+    });
