7539af303b318cd3a4a6660c0837373fa9133821
emirubinsky <rubinskyemiliano@gmail.com>
2024-04-13 19:46:48 -0300

    Arreglos getProducts

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -3,6 +3,9 @@ import cartController from "../dao/controllers/cart.controller.js";
 
 const cartRouter = express.Router();
 
+// Maneja la solicitud de busca de carritos
+cartRouter.get("/", cartController.getCarts);
+
 // Maneja la solicitud de renderizar el carrito
 cartRouter.get("/:cid", cartController.getCartById);
 
@@ -10,20 +13,14 @@ cartRouter.get("/:cid", cartController.getCartById);
 // Maneja la solicitud para comprar productos
 cartRouter.post("/products/buy", cartController.buyCart);
 */
-
 // Maneja la solicitud de agregar el producto al carrito
 cartRouter.post("/", cartController.addProductToCart);
-
 // Maneja la solicitud para actualizar el carrito con nuevos productos
 cartRouter.put("/:cid", cartController.updateCart);
-
 // Maneja la solicitud para actualizar la cantidad de algun producto dentro del carrito
 cartRouter.put("/:cid/products/:pid", cartController.updateProductQuantityInCart);
-
 // Maneja la solicitud para borrar el producto seleccionado del carrito
 cartRouter.delete("/:cid/products/:pid", cartController.deleteProductFromCart);
-
 // Maneja la solicitud para limpiar el carrito
 cartRouter.delete("/:cid", cartController.clearCart);
-
 export default cartRouter;
